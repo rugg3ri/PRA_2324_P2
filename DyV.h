ef550808adf0e4608ba9de54139c4624cd0058ad
RNOGMON <rnogmon@alumno.upv.es@ldsic-vdi04.upvnet.upv.es>
2025-10-01 00:38:44 +0200

    Añade busqueda en descendente

--- DyV.h
+++ DyV.h
@@ -16,3 +16,19 @@ int BusquedaBinaria(T x,const vector<T>& v, int ini, int fin){
 		return BusquedaBinaria(x, v, medio+1, fin);
 	}
 }
+
+template<typename T>
+int BusquedaBinaria_INV(T x,const vector<T>& v, int ini, int fin){
+        if(ini > fin){
+                return -1;
+        }
+        int medio = (ini + fin) / 2;
+        if(v[medio] == x){
+                return medio;
+        }else if(v[medio] < x){
+                return BusquedaBinaria_INV(x, v, ini, medio-1);
+        }else{
+                return BusquedaBinaria_INV(x, v, medio+1, fin);
+        }
+}
+
--- test_DyV.cpp
+++ test_DyV.cpp
@@ -9,7 +9,7 @@ int main(){
 	int posInt = BusquedaBinaria(9, vecInt, 0, vecInt.size() - 1);
 	cout <<"Posicion del 9 en vecInt: " << posInt << endl;
 
-	vector<float>vecFlt = {1.2f, 4.5f, 3.5f, 7.8f, 2.2f};
+	vector<float>vecFlt = {1.2f, 2.5f, 3.5f, 7.8f, 10.2f};
 	int posFlt = BusquedaBinaria(1.2f, vecFlt, 0, vecFlt.size() - 1);
 	cout << "Posicion del 1.2f en vecFlt: " << posFlt << endl;
 
@@ -21,6 +21,23 @@ int main(){
 	int posChr = BusquedaBinaria('d', vecChr, 0, vecChr.size() - 1);
 	cout << "Posicion del 'd' en vecChr: " << posChr << endl;
 	
+	vector<int>vecDescInt = {20, 15, 12, 10, 5};
+        int posDescInt = BusquedaBinaria_INV(10, vecDescInt, 0, vecDescInt.size() - 1);
+        cout <<"Posicion del 9 en vecDescInt: " << posDescInt << endl;
+
+        vector<float>vecDescFlt = {10.3f, 8.5f, 7.8f, 4.5f, 1.3f};
+        int posDescFlt = BusquedaBinaria_INV(1.3f, vecDescFlt, 0, vecDescFlt.size() - 1);
+        cout << "Posicion del 1.3f en vecDescFlt: " << posDescFlt << endl;
+
+        vector<string>vecDescStr = {"hola", "adios", "chao", "yes"};
+        int posDescStr = BusquedaBinaria_INV(string("adios"), vecDescStr, 0,vecDescStr.size() - 1);
+cout << "Posicion de \"adios\" en vecDescStr: " << posDescStr << endl;
+
+        vector<char>vecDescChr = {'e', 'd', 'c', 'b', 'a'};
+        int posDescChr = BusquedaBinaria_INV('b', vecDescChr, 0, vecDescChr.size() - 1);
+        cout << "Posicion del 'b' en vecDescChr: " << posDescChr << endl;
+
+
 	return 0;
 }
 		
